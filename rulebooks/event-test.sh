eb65c6845cc3dbe76b6bfce1b78c8ce86736580d
Built By Ansible Automation <dataylor@test123.tfound.org>
2025-05-17 10:00:35 -0400

    adding eda config import and event testing script

--- rulebooks/event-test.sh
+++ rulebooks/event-test.sh
@@ -1,6 +1,25 @@
-#!/bin/bash
-curl \
+#!/bin/bash -x
+host="https://abc:123@aap25-eu-aap-west.apps.compute7-cluster.tfound.org"
+edastream="eda-event-streams/api/eda/v1/external_event_stream/69ccc3cc-b0d6-47d3-8fab-0a29d966a6ce/post/"
+msg="test123 from the cmd line"
+event="null event"
+status="null status"
+if [[ ! -z "$1" ]]
+then
+  event="$1"
+fi
+
+if [[ ! -z "$2" ]]
+then
+  status="$2"
+fi
+
+if [[ ! -z "$3" ]]
+then
+  msg="$3"
+fi
+curl -k\
 	--header "Content-Type: application/json" \
 	--request POST \
-	--data '{"log_status": true, "message": "test123"}' \
-	localhost:5000
+	--data '{"webhook_event": "'"${event}"'", "status": "'"${status}"'", "message": "'"${msg}"'"}' \
+	${host}/${edastream}
